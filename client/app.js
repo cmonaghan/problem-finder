28df8d57cc1e9df984129b5f755c58c7dc80943c
Christian Monaghan <christian.monaghan@gmail.com>
2013-12-16 14:21:05 -0800

    Problems are displayed with real-time updating

--- client/app.js
+++ client/app.js
@@ -1,14 +1,17 @@
+var ref = new Firebase("https://problemfinder.firebaseio.com/problemList");
+
 angular.module('problemFinderApp', ['firebase'])
 .controller('submitProblem', function($scope, $firebase){
 
-  var ref = new Firebase("https://problemfinder.firebaseio.com/problemList");
   $scope.problemList = $firebase(ref);
 
   $scope.saveProblem = function(){
     $scope.problemList.$add({body: $scope.newProblem})
     $scope.newProblem = '';
-  }
-});
-// .controller('problemView', function($scope){
+  };
+})
+.controller('problemView', function($scope, $firebase){
 
-// });
+  $scope.problemList = $firebase(ref);
+
+});
