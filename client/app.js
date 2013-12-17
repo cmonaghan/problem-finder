83b962678095cf82603529cc1e584426b4b46c51
Christian Monaghan <christian.monaghan@gmail.com>
2013-12-16 20:04:53 -0800

    Added upvote arrow and registers a click event.

--- client/app.js
+++ client/app.js
@@ -2,16 +2,17 @@ var ref = new Firebase("https://problemfinder.firebaseio.com/problemList");
 
 angular.module('problemFinderApp', ['firebase'])
 .controller('submitProblem', function($scope, $firebase){
-
   $scope.problemList = $firebase(ref);
 
   $scope.saveProblem = function(){
-    $scope.problemList.$add({body: $scope.newProblem})
+    var timestamp = new Date();
+    $scope.problemList.$add({body: $scope.newProblem, dateAdded: timestamp, voteCount: 0})
     $scope.newProblem = '';
   };
 })
 .controller('problemView', function($scope, $firebase){
-
   $scope.problemList = $firebase(ref);
-
+  $scope.upvote = function(){
+    console.log('something was clicked');
+  }
 });
